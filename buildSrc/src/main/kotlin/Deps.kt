567e5aef4ab157efe643875b3abe8c4e1376724a
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-12-27 21:03:14 +0700

    kotlin 1.4.30

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -3,8 +3,8 @@
  */
 
 object Deps {
-    private const val kotlinVersion = "1.4.10"
-    private const val androidGradleVersion = "4.0.1"
+    private const val kotlinVersion = "1.4.30-M1"
+    private const val androidGradleVersion = "4.1.1"
 
     private const val androidAppCompatVersion = "1.1.0"
     private const val androidCoreTestingVersion = "2.1.0"
@@ -12,10 +12,10 @@ object Deps {
 
     private const val kotlinPoetVersion = "1.3.0"
 
-    private const val coroutinesVersion = "1.3.9"
-    private const val mokoGraphicsVersion = "0.4.0"
-    private const val mokoResourcesVersion = "0.13.0"
-    const val mokoUnitsVersion = "0.4.1"
+    private const val coroutinesVersion = "1.4.2"
+    private const val mokoGraphicsVersion = "0.5.1"
+    private const val mokoResourcesVersion = "0.13.3"
+    const val mokoUnitsVersion = "0.4.2"
 
     object Android {
         const val compileSdk = 29
